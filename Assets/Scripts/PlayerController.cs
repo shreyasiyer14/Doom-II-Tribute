46c88c686af1f9b354b2df7884063b3ec4424253
l0ftyWhizZ <shreyasiyer14@gmail.com>
2017-12-11 01:21:36 +0530

    Started implementation of player controller class.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private int vertAngleIndex;
+
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat (anim.GetParameter(vertAngleIndex).nameHash, 48.0f * Input.mousePosition.y/Screen.height);
+	}
+}
