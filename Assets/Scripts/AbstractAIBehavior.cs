43a27b0ec867af1af0e7428df1840bcdfe9c3af2
l0ftyWhizZ <shreyasiyer14@gmail.com>
2017-12-29 00:39:10 +0530

    More updates done in the enum usage.

--- Assets/Scripts/AbstractAIBehavior.cs
+++ Assets/Scripts/AbstractAIBehavior.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 abstract public class AbstractAIBehavior : MonoBehaviour {
-	protected enum Direction {Forward, ForwardLeft, Left, BackwardLeft, Back, BackwardRight, Right, ForwardRight, Default};
+	protected enum Direction : int {Forward, Back, Left, Right, ForwardLeft, ForwardRight, BackwardLeft, BackwardRight, Default};
 
 	protected float GetDotWithPlayer(Transform playerCamera) {
 		float dot = Vector3.Dot (playerCamera.forward, transform.forward)/(playerCamera.forward.magnitude * transform.forward.magnitude);
@@ -12,15 +12,15 @@ abstract public class AbstractAIBehavior : MonoBehaviour {
 
 	protected Direction ChangeSpriteCode (float dotValue) {
 		if (dotValue <= 1.0f && dotValue > 0.71f) {
-			return Direction.Forward;
+			return Direction.Back;
 		} else if (dotValue <= 0.71f && dotValue > 0.1f) {
-			return Direction.ForwardLeft;
+			return Direction.BackwardLeft;
 		} else if (dotValue <= 0.1f && dotValue >= 0.0f) {
 			return Direction.Left;
 		} else if (dotValue < 0.0f && dotValue > -0.71f) {
-			return Direction.BackwardLeft;
+			return Direction.ForwardLeft;
 		} else if (dotValue <= -0.71f && dotValue > -1.0f) {
-			return Direction.Back;
+			return Direction.Forward;
 		}
 		return Direction.Default;
 	}
--- Assets/Scripts/ImpController.cs
+++ Assets/Scripts/ImpController.cs
@@ -1,17 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ImpController : AbstractAIBehavior {
 	private Transform player;
 	private Direction spriteCode;
+	private Animator anim;
+
+	private List<Direction> parameterList = new List<Direction> ();
 
 	void Start () {
-		player = Camera.main.transform;	
+		player = Camera.main.transform;
+		anim = GetComponent<Animator> ();
+		parameterList.Add (Direction.Forward);
+		parameterList.Add (Direction.Back);
+		parameterList.Add (Direction.Left);
+		parameterList.Add (Direction.Right);
+		parameterList.Add (Direction.ForwardLeft);
+		parameterList.Add (Direction.ForwardRight);
+		parameterList.Add (Direction.BackwardLeft);
+		parameterList.Add (Direction.BackwardRight);
+	}
+
+	private void ResetAllParams () {
+		if (parameterList.Count == 0)
+			return;
+		foreach (Direction param in parameterList) {
+			if (param != spriteCode)
+				anim.SetBool (anim.GetParameter(Array.IndexOf(Enum.GetValues(param.GetType()), param)).name, false);
+		}
+		return;
 	}
 	
 	void Update () {
-		transform.LookAt (player.forward);
+		transform.LookAt (player);
 		spriteCode = ChangeSpriteCode (GetDotWithPlayer (player));
+
+		ResetAllParams ();
+		anim.SetBool (anim.GetParameter(Array.IndexOf(Enum.GetValues(spriteCode.GetType()), spriteCode)).name, true);
 	}
 }
