a6971da7728f7181d74c2778633629f6a397cea5
l0ftyWhizZ <shreyasiyer14@gmail.com>
2017-12-30 21:20:25 +0530

    Changed Imp projectile sprite.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,13 +6,20 @@ public class Projectile : MonoBehaviour {
 	[SerializeField] private float damage;
 	[SerializeField] private float speed;
 
+	private Animator anim;
+
 	void Awake () {
+		anim = transform.GetChild (0).gameObject.GetComponent<Animator> ();
 		Destroy (gameObject, 50f);
 		InvokeRepeating ("ProjectileTranslation", 0.05f, 0.2f);
 	}
 
 	void ProjectileTranslation () {
 		transform.GetChild (0).LookAt (Camera.main.transform);
-		transform.Translate (Vector3.forward * speed * Time.fixedDeltaTime * speed/5.0f);		
+		transform.Translate (Vector3.forward * speed * Time.fixedDeltaTime * speed/5.0f);
+		if (Vector3.Distance (Camera.main.transform.position, transform.position) <= 5.0f) {
+			anim.SetBool (anim.GetParameter(0).name, true);
+			Destroy (gameObject, 2f);
+		}
 	}
 }
