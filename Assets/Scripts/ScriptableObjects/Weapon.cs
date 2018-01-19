b3868c85ddc74b4eea931bc50cd24a7694ac69a9
Shreyas Iyer <Shreyas.Srinivasan@iiitb.org>
2018-01-19 19:39:26 +0530

    Added a pistol weapon object. Need to work on adding additional fire animations.

--- Assets/Scripts/ScriptableObjects/Weapon.cs
+++ Assets/Scripts/ScriptableObjects/Weapon.cs
@@ -7,7 +7,7 @@ public class Weapon : ScriptableObject {
 	public string name;
 	public float damage;
 	public AnimationClip fireAnimation;
-	public Sprite fireAdditionalSprite;
+	public AnimationClip fireAdditionalAnimation;
 	public Sprite mainSprite;
 	public Sprite droppedSprite;
 	public bool isSingleShot;
