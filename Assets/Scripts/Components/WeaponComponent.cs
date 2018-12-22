017d69a43758c55411d002b5f38463a225569964
Shreyas Iyer <shreyasiyer14@gmail.com>
2018-12-23 03:17:16 +0530

    Started fixing the fire animator states.

--- Assets/Scripts/Components/WeaponComponent.cs
+++ Assets/Scripts/Components/WeaponComponent.cs
@@ -4,33 +4,16 @@ using UnityEngine;
 
 public class WeaponComponent : MonoBehaviour {
 	public Weapon currentWeapon;
-
 	private Animator anim;
-	private Animator fireAdditionalAnim;
 
 	void Start () {
 		anim = GetComponent<Animator> ();
-		fireAdditionalAnim = transform.GetChild (0).GetComponent<Animator> ();
-		fireAdditionalAnim.enabled = false;
-		anim.enabled = false;
 		GetComponent<SpriteRenderer> ().sprite = currentWeapon.mainSprite;
 	}
 
 	public void onFireEvent (int isFiring) {
-		if (isFiring == 1) {
-			anim.enabled = true;
-	
-			anim.SetTrigger (currentWeapon.fireAnimation.name);
-
-			if (currentWeapon.fireAdditionalAnimation != null) {
-				fireAdditionalAnim.enabled = true;
-				fireAdditionalAnim.SetTrigger (currentWeapon.fireAdditionalAnimation.name);
-			}
-		} else {
-			if (anim.enabled) {
-				anim.enabled = false;
-				fireAdditionalAnim.enabled = false;
-			}
+		if (isFiring == 1) {	
+			anim.Play (currentWeapon.fireAnimation.name);
 		}
 		GetComponent<SpriteRenderer> ().sprite = currentWeapon.mainSprite;
 	}
--- Assets/Scripts/ScriptableObjects/Weapon.cs
+++ Assets/Scripts/ScriptableObjects/Weapon.cs
@@ -7,7 +7,7 @@ public class Weapon : ScriptableObject {
 	public string name;
 	public float damage;
 	public AnimationClip fireAnimation;
-	public AnimationClip fireAdditionalAnimation;
+	// public AnimationClip fireAdditionalAnimation;
 	public Sprite mainSprite;
 	public Sprite droppedSprite;
 	public bool isSingleShot;
